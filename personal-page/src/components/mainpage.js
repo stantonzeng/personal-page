f034ab6a4ee917819dc7ca68b2c54e0c3df33ea2
Stanton Zeng <44949352+stantonzeng@users.noreply.github.com>
2022-06-23 04:11:29 -0700

    Fixed projects

--- personal-page/src/components/mainpage.js
+++ personal-page/src/components/mainpage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import { useSpring, animated } from '@react-spring/web'
 import './mainpage.css'
@@ -6,13 +6,30 @@ import './mainpage.css'
 const title = 0
 const title_speed = 1.3
 
-const pg_one = 0.6
+const pg_one = 0.9
 const pg_one_speed = 1.5
 
-const pg_two = pg_one+1
+const pg_two = pg_one+0.1
 const pg_two_speed = 1.5
 
+const opacity_val = 0.1
+
 var locat = 0
+
+function checkBound(placement){
+  if(placement*500 <= locat && (placement+0.75)*500 >= locat){
+    return true;
+  }
+  return false;
+}
+
+function checkBoundProj(placement){
+  if(placement*500 <= locat && (placement+1.5)*500 >= locat){
+    return true;
+  }
+  return false;
+}
+
 
 export function Mainpage(){
   
@@ -20,26 +37,34 @@ export function Mainpage(){
 
   const [styles_title, api_title] = useSpring(() => ({ opacity: 1 }))
   const [styles_one, api_one] = useSpring(() => ({ opacity: 0.5 }))
+  const [styles_two, api_two] = useSpring(() => ({ opacity: 0.5 }))
 
   const parallax = useRef();
 
   const handleScroll = () => {
     if (parallax.current) {
       console.log(parallax.current.current)
-      locat = parallax.current.current + 100;
-
-      if(title*500 <= locat && (title+1)*500 >= locat){
+      locat = parallax.current.current + 250;
+      
+      if(checkBound(title)){
         api_title.start({opacity : 1})
       }
       else{
-        api_title.start({opacity : 0.5})
+        api_title.start({opacity : opacity_val})
       }
     
-      if(pg_one*500 <= locat && (pg_one+1)*500 >= locat){
+      if(checkBound(pg_one)){
         api_one.start({opacity : 1})
       }
       else{
-        api_one.start({opacity : 0.5})
+        api_one.start({opacity : opacity_val})
+      }
+
+      if(checkBoundProj(pg_two+1)){
+        api_two.start({opacity : 1})
+      }
+      else{
+        api_two.start({opacity : opacity_val})
       }
     }
   }
@@ -53,16 +78,16 @@ export function Mainpage(){
   }, [])
 
   return(
-    <Parallax ref = {parallax} className = 'test_layer' pages = {2}>
+    <Parallax ref = {parallax} className = 'test_layer' pages = {3}>
       <ParallaxLayer speed={title_speed} style={{
       display: 'flex',
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center'  
     }}>
 
       <animated.div style = {styles_title}>
         <h1 className = "title">Stanton Zeng</h1>
-        <p className = "subTitle"> Software and Math</p>
+        {/* <p className = "subTitle"> Software and Math</p> */}
       </animated.div>
       
       </ParallaxLayer>
@@ -86,19 +111,17 @@ export function Mainpage(){
       
       {/* Projects START */}
 
-      {/* <ParallaxLayer sticky = {{start: pg_two, end: pg_two+1}} offset = {pg_two} speed = {pg_two_speed} >
-        <h1 className = "projects_title">Projects</h1>
-        
+      <ParallaxLayer offset = {pg_two} speed = {pg_two_speed} >
+        <animated.div style = {styles_two}>
+          <h1 className = "projects_title">Projects</h1>
+            <div className = "project_grid">
+                <div className = "proj1">Project 1</div>
+                <div className = "proj2">Project 2</div>
+                <div className = "proj3">Project 3</div>
+                <div className = "proj4">Project 4</div>
+            </div>
+        </animated.div>
       </ParallaxLayer>
-      <ParallaxLayer sticky = {{start: pg_two+0.5, end: pg_two+1}} offset = {pg_two+0.5} style = {{...alignCenter}}>
-      
-      <div className = "text1">
-        <div className = "project_grid">
-            <div className = "proj1">hello</div>
-        </div>
-      </div>
-      
-      </ParallaxLayer> */}
 
       {/* Projects End */}
 
